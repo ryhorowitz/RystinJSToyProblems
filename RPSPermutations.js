e00753f8174b3c36940c5799a1d3b8cb768844e7
ryhorowitz <ryan.w.horowitz@gmail.com>
2020-12-21 14:14:34 -0500

    Solved RPS Permutations with Loops O(n**2)

--- RPSPermutations.js
+++ RPSPermutations.js
@@ -16,3 +16,25 @@
 * Example:
 * rockPaperScissors(5); // => ['RRRRR', 'RRRRP', 'RRRRS', etc...] follow this pattern for psuedo coding []
 * // IOCE - INPUT / OUTPUT / CONSTRAINTS / EDGE-CASES
+*/
+
+function rockPaperScissors() {    //This answers complexity is O(n**2)!! We must refactor!!!
+  let possibleMoves = [['R', 'P', 'S'], ['R', 'P', 'S'], ['R', 'P', 'S']]; 
+  let moveCombos = []; 
+  let move = ''
+  for (var i = 0; i < possibleMoves[0].length; i++) {
+    let round1Outcome = possibleMoves[0][i]; //'r' or 'p' or 's'
+        
+    for (var j = 0; j < possibleMoves[1].length; j++) {
+      let round2Outcome = possibleMoves[1][j];   
+      
+      for (var k = 0; k < possibleMoves[2].length; k++) {
+        let round3Outcome = possibleMoves[2][k];
+        let combo = round1Outcome + round2Outcome + round3Outcome; 
+        moveCombos.push(combo); 
+      }
+    }
+  }
+  return moveCombos; 
+}; 
+console.log(rockPaperScissors());
