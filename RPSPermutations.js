5e5639bc238ae9b4a3859e66b9f5cb73c6e16f41
ryhorowitz <ryan.w.horowitz@gmail.com>
2020-12-21 01:02:57 -0500

    Added RPS Permutations problem to repo

---
+++ RPSPermutations.js
@@ -0,0 +1,18 @@
+/*
+* Write a function that generates every sequence of throws a single
+* player could throw over a three-round game of rock-paper-scissors.
+*
+* Your output should look something like:
+*   ["RRR",
+*    "RRP",
+*    "RRS",
+*    "RPR",
+*    ...etc...
+*   ]
+*
+* Extra credit:
+*   - Make your function return answers for any number of rounds.
+*
+* Example:
+* rockPaperScissors(5); // => ['RRRRR', 'RRRRP', 'RRRRS', etc...] follow this pattern for psuedo coding []
+* // IOCE - INPUT / OUTPUT / CONSTRAINTS / EDGE-CASES
